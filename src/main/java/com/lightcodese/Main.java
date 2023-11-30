079b6f33a41f5232b27afe07d5582cbfaf898a49
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-30 11:24:00 +0700

    rest api

--- src/main/java/com/lightcodese/Main.java
+++ src/main/java/com/lightcodese/Main.java
@@ -2,10 +2,20 @@ package com.lightcodese;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+    @GetMapping("/")
+    public GreetingResponse greet(){
+        return new GreetingResponse("Hello");
+    }
+
+    record GreetingResponse(String str){}
 }
