9d7f8ac41eeabbf13a4272d93f9b0ba36cf8ad96
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-11-30 11:30:10 +0700

    return json

--- src/main/java/com/lightcodese/Main.java
+++ src/main/java/com/lightcodese/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -17,5 +19,35 @@ public class Main {
         return new GreetingResponse("Hello");
     }
 
-    record GreetingResponse(String str){}
+    class GreetingResponse {
+        private final String greet;
+
+        GreetingResponse(String greet) {
+            this.greet = greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetingResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetingResponse that = (GreetingResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+    }
 }
