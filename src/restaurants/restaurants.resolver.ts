c848ebe29df295527f90a64a0fcd4b5d3303f01a
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-15 19:09:42 +0700

    graphql ex

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,10 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantsResolver {
-  @Query((returns) => Boolean)
-  isPizzaGood() {
+  @Query((returns) => Restaurant)
+  myRestaurant() {
     return true;
   }
 }
