26046801f345a1f1b370a3b1ecd53ecdae85e68f
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-15 20:07:22 +0700

    connect db through typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'god_eats',
+      password: 'god_eats',
+      database: 'god_eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
