062e1ee562bcbfd6e57393d43cf8f319d57a611b
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-15 20:38:50 +0700

    load entity

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,32 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity({
+  name: 'restaurants',
+})
 export class Restaurant {
+  @Field((type) => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
 
   @Field((type) => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   ownersName: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
