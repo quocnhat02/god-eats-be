ccbab140170a9636cde3a5f61c53a5cde07b8f63
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-15 18:58:30 +0700

    create a new resolver to handle graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
