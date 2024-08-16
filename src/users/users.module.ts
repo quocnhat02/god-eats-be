cddb1469e1af3d595045645816cec40343d0ab11
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-08-16 15:32:23 +0700

    config and interface

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from 'src/jwt/jwt.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
